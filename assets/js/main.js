33ddf4a2005417ca106104337c9f3ddab6ff0af0
Samanta Caiulo <samantacaiulo@gmail.com>
2024-03-30 22:03:57 +0100

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () 
 {
-    var counterElement = document.getElementById('counter-value');
-    var incrementButton = document.getElementById('btnIncrement');
-    var decrementButton = document.getElementById('btnDecrement');
-    var resetButton = document.getElementById('btnReset');
-    var buttonsContainer = document.getElementById('buttons');
+    
+    var counterMain = document.getElementById('counter');
+    counterMain.classList.add('counter');
+
+    var counterElement = document.createElement('h1');
+    counterElement.textContent = '0';
+    counterElement.id = 'counter-value';
+    counterMain.appendChild(counterElement);
+
+    var buttonsContainer = document.createElement('div');
+    buttonsContainer.id = 'buttons';
+    buttonsContainer.classList.add('buttons');
+    counterMain.appendChild(buttonsContainer);
+
+    var incrementButton = document.createElement('button');
+    incrementButton.textContent = '+';
+    incrementButton.id = 'btnIncrement';
+    buttonsContainer.appendChild(incrementButton);
+
+    var decrementButton = document.createElement('button');
+    decrementButton.textContent = '-';
+    decrementButton.id = 'btnDecrement';
+    buttonsContainer.appendChild(decrementButton);
+
+    var resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.id = 'btnReset';
+    buttonsContainer.appendChild(resetButton);
 
     incrementButton.addEventListener('click', function () {
         counterElement.innerText = parseInt(counterElement.innerText) + 1;
