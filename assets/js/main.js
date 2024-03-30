b2b891540de35272a88773270f2b9521500f38f1
Samanta Caiulo <samantacaiulo@gmail.com>
2024-03-30 22:10:08 +0100

    Const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () 
 {
-    
-    var counterMain = document.getElementById('counter');
+
+    const counterMain = document.getElementById('counter');
     counterMain.classList.add('counter');
 
-    var counterElement = document.createElement('h1');
+    const counterElement = document.createElement('h1');
     counterElement.textContent = '0';
     counterElement.id = 'counter-value';
     counterMain.appendChild(counterElement);
 
-    var buttonsContainer = document.createElement('div');
+    const buttonsContainer = document.createElement('div');
     buttonsContainer.id = 'buttons';
     buttonsContainer.classList.add('buttons');
     counterMain.appendChild(buttonsContainer);
 
-    var incrementButton = document.createElement('button');
+    const incrementButton = document.createElement('button');
     incrementButton.textContent = '+';
     incrementButton.id = 'btnIncrement';
     buttonsContainer.appendChild(incrementButton);
 
-    var decrementButton = document.createElement('button');
+    const decrementButton = document.createElement('button');
     decrementButton.textContent = '-';
     decrementButton.id = 'btnDecrement';
     buttonsContainer.appendChild(decrementButton);
 
-    var resetButton = document.createElement('button');
+    const resetButton = document.createElement('button');
     resetButton.textContent = 'Reset';
     resetButton.id = 'btnReset';
     buttonsContainer.appendChild(resetButton);
